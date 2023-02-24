948a2c5bda67cfa7c898c31a2213a54d0455e20d
Tatiana Obando <PATH="/opt/homebrew/bin:$PATH>
2023-02-24 12:48:32 -0800

    excuse generator

--- src/App.js
+++ src/App.js
@@ -12,27 +12,50 @@ function App() {
   // useEffect(() => {
   //   fetchCatFact();
   // }, []);
-  const [name, setName] =useState("");
-  const [predictedAge, setPredictedAge] = useState(null);
 
-  const fetchData = () => {
-    Axios.get(`https://api.agify.io/?name=${name}`).then((res)=> {
-      setPredictedAge(res.data);
-    });
-  };
+  // const [name, setName] =useState("");
+  // const [predictedAge, setPredictedAge] = useState(null);
+
+  // const fetchData = () => {
+  //   Axios.get(`https://api.agify.io/?name=${name}`).then((res)=> {
+  //     setPredictedAge(res.data);
+  //   });
+  // };
+    const [generatedExcuse, setExcuse] = useState("");
+
+
+    const fetchData = (excuse) => {
+      Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}`).then((res) => {
+        setExcuse(res.data[0].excuse);
+      }
+      )
+    }
+
+
+
 
   return (
     <div className="App">
 
       {/* <button onClick={fetchCatFact}>Generate Cat Fact</button>
       <p> {catFact}</p> */}
-      <input placeholder='write a name...' 
+
+      {/* <input placeholder='write a name...' 
       onChange={(event) => { setName(event.target.value);
       }}/>
       <button onClick={fetchData}>Predict Age</button>
       <h1> Name: {predictedAge?.name} </h1>
       <h1> Predicted age: {predictedAge?.age} </h1>
-      <h1> Count: {predictedAge?.count} </h1>
+      <h1> Count: {predictedAge?.count} </h1> */}
+
+      <h1> Generate an excuse </h1>
+      <button onClick={() => fetchData("party")} > Party </button>
+      <button onClick={() => fetchData("family")} > Family </button>
+      <button onClick={() => fetchData("office")} > Office </button>
+      <p>{generatedExcuse}</p>
+
+
+
     </div>
   );
 }
